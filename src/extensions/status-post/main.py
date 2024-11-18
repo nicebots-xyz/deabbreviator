0cfd2fee3434e91e0961da3a87a0f9727cf9e3a3
Paillat <me@paillat.dev>
2024-11-18 10:03:29 +0100

    :goal_net: Better error handling of status push

--- src/extensions/status-post/main.py
+++ src/extensions/status-post/main.py
@@ -1,6 +1,9 @@
 # Copyright (c) NiceBots.xyz
 # SPDX-License-Identifier: MIT
 
+import math
+from typing import Any
+
 import aiohttp
 import discord
 from discord.ext import commands, tasks
@@ -24,16 +27,16 @@ schema = Schema(
 
 
 class Status(commands.Cog):
-    def __init__(self, bot: discord.Bot, config: dict) -> None:
-        self.bot = bot
-        self.config = config
-        self.push_status_loop = tasks.loop(seconds=self.config["every"])(self.push_status_loop)
+    def __init__(self, bot: discord.Bot, config: dict[Any, Any]) -> None:
+        self.bot: discord.Bot = bot
+        self.config: dict[Any, Any] = config
+        self.push_status_loop: tasks.Loop = tasks.loop(seconds=self.config["every"])(self.push_status_loop_meth)  # pyright: ignore [reportMissingTypeArgument]
 
     @commands.Cog.listener(once=True)
     async def on_ready(self) -> None:
         self.push_status_loop.start()
 
-    async def push_status_loop(self) -> None:
+    async def push_status_loop_meth(self) -> None:
         try:
             await self.push_status()
             logger.info("Pushed status.")
@@ -41,10 +44,14 @@ class Status(commands.Cog):
             logger.exception("Failed to push status.")
 
     async def push_status(self) -> None:
-        ping = str(round(self.bot.latency * 1000))
+        latency = self.bot.latency
+        if latency == float("inf") or math.isnan(latency):
+            logger.warning("Latency is infinite or NaN, skipping status push.")
+            return
+        ping = str(round(latency * 1000))
         async with aiohttp.ClientSession() as session, session.get(self.config["url"] + ping) as resp:
             resp.raise_for_status()
 
 
-def setup(bot: discord.Bot, config: dict) -> None:
+def setup(bot: discord.Bot, config: dict[Any, Any]) -> None:
     bot.add_cog(Status(bot, config))
